175a0cade395a78fa9b6084d88fc7012c55e0f00
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-03-27 17:52:14 -0400

    crud endpoints working including get student by cohort, migrations for cohort/students, seeding completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server');
 
 const port = 4000;
-server.listen(port, () => console.log(`\ Web API Listening on http://localhost:4000/${port} \n`));
+server.listen(port, () => console.log(`\ Web API Listening on http://localhost:${port} \n`));
